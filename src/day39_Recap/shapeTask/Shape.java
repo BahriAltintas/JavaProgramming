340c593117bf1f1dd621c65181a9943da9466c4b
Bahri Altıntaş <bahrialtintas@gmail.com>
2022-04-16 02:07:35 +0300

    Day 39. Encapsulation, Inheritance Recaps

---
+++ src/day39_Recap/shapeTask/Shape.java
@@ -0,0 +1,22 @@
+package day39_Recap.shapeTask;
+
+public class Shape {
+
+    public String name;
+
+
+
+
+}
+/*variables:
+        name
+
+        Encapsulate the field
+
+        Add a constructor to set the filed
+
+        Methods:
+        area(){}
+        perimeter(){}
+
+ */
