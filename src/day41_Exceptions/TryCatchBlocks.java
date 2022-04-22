fdfab67eec4ff6197c857ecca44df144292d5f0d
Bahri Altıntaş <bahrialtintas@gmail.com>
2022-04-23 01:53:02 +0300

    Day 41. Exceptions

--- src/day41_Exceptions/TryCatchBlocks.java
+++ src/day41_Exceptions/TryCatchBlocks.java
@@ -1,5 +1,8 @@
 package day41_Exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class TryCatchBlocks {
 
     public static void main(String[] args) {
@@ -58,11 +61,28 @@ public class TryCatchBlocks {
 
         System.out.println("Test3 Completed");
 
+        System.out.println("--------------------------------------------------");
 
 
+        System.out.println("Hello");
 
 
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+
+        System.out.println("Cydeo");
+
+        System.out.println("--------------------------------------------------");
+
+        try {
+            FileInputStream file = new FileInputStream("File Path");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
 
     }
