cd04cbbd34bb6a450c128f4f4634647fb3b3a8fc
Bahri Altıntaş <bahrialtintas@gmail.com>
2022-05-01 23:36:02 +0300

    Day 44. Abstraction and Interface Intro

---
+++ src/day44_Abstract/animalTask/Dog.java
@@ -0,0 +1,26 @@
+package day44_Abstract.animalTask;
+
+public final class Dog extends Animal{
+
+    public Dog(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+
+    public void bark(){
+        System.out.println(getName() + " is barking");
+    }
+
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is eating Pizza");
+    }
+
+
+    public void play(){
+        System.out.println(getName() + " is playing with ball");
+    }
+
+
+}
