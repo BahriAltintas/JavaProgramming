a8649e5e2bc3c51826cce67de333ab253f4454a4
Bahri Altıntaş <bahrialtintas@gmail.com>
2022-05-10 01:55:39 +0300

    Day 45. Abstraction and Interface

--- src/day44_Abstraction/animalTask/Cat.java
+++ src/day44_Abstraction/animalTask/Cat.java
@@ -1,6 +1,6 @@
 package day44_Abstraction.animalTask;
 
-public final class Cat extends Animal{
+public final class Cat extends Animal implements Playable{
 
     public Cat(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
@@ -16,6 +16,12 @@ public final class Cat extends Animal{
 
     public void meow(){
         System.out.println(getName() + " is meowing");
+    }
+
+
+    @Override
+    public void play() {
+
     }
 
 
--- src/day44_Abstraction/animalTask/Parrot.java
+++ src/day44_Abstraction/animalTask/Parrot.java
@@ -1,6 +1,6 @@
 package day44_Abstraction.animalTask;
 
-public final class Parrot extends Animal{
+public final class Parrot extends Animal implements Playable{
 
 
     public Parrot(String name, String breed, char gender, int age, String size, String color) {
@@ -14,4 +14,8 @@ public final class Parrot extends Animal{
         System.out.println(getName()+" is eating chocolate");
     }
 
+    @Override
+    public void play() {
+
+    }
 }
