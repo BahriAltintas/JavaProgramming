27e69afe970298d369fe2711cd07e350ff8eac9b
Bahri Altıntaş <bahrialtintas@gmail.com>
2022-05-22 22:49:07 +0300

    Day 47. Polymorphism

---
+++ src/day47_Polymoprhism/EncapsulationReview.java
@@ -0,0 +1,38 @@
+package day47_Polymoprhism;
+
+public class EncapsulationReview {
+
+    private String bookTitle;
+    private final String publishData;
+
+    public EncapsulationReview(String bookTitle, String publishData) {
+        setBookTitle(bookTitle);
+        this.publishData = publishData;
+    }
+
+    public String getBookTitle() {
+
+        if (bookTitle == null){
+            return "";
+        }
+
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+
+        if (bookTitle == null || bookTitle.isEmpty()){
+            throw new RuntimeException("Invalid Data");
+        }
+
+        this.bookTitle = bookTitle;
+    }
+
+    public String getPublishData() {
+        return publishData;
+    }
+
+
+
+
+}
