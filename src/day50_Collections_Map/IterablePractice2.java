8ba650e284efe1a6c87bab7aa90e7bc12c119f6a
Bahri Altıntaş <bahrialtintas@gmail.com>
2022-06-06 01:50:52 +0300

    Day 50. Collections_Map

---
+++ src/day50_Collections_Map/IterablePractice2.java
@@ -0,0 +1,66 @@
+package day50_Collections_Map;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class IterablePractice2 {
+
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+        names.addAll(Arrays.asList(
+                "Ahmed", "ahmed", "aHmEd", "John", "Ercon", "Daniel", "Mustafa", "Mohammed", "Ahmed", "ahmed", "Yuliia", "Chris"
+        ));
+
+        //remove all the names "ahmed"
+
+        Iterator<String> it = names.iterator();
+
+        while (it.hasNext()){
+            if (it.next().equalsIgnoreCase("ahmed")){
+                it.remove();
+            }
+        }
+
+        System.out.println(names);
+
+        System.out.println("-------------------------------------------------------------");
+
+        List<String> names2 = new ArrayList<>();
+        names2.addAll(Arrays.asList(
+                "Ahmed", "ahmed", "aHmEd", "John", "Ercon", "Daniel", "Mustafa", "Mohammed", "Ahmed", "ahmed", "Yuliia", "Chris"
+        ));
+
+        //remove all the names "ahmed"
+
+        for ( Iterator<String> it2 = names2.iterator() ; it2.hasNext() ; ){
+            if (it2.next().equalsIgnoreCase("ahmed")){
+                it2.remove();
+            }
+        }
+
+        System.out.println(names2);
+
+        System.out.println("-------------------------------------------------------------");
+
+        List<String> names3 = new ArrayList<>();
+        names3.addAll(Arrays.asList(
+                "Ahmed", "ahmed", "aHmEd", "John", "Ercon", "Daniel", "Mustafa", "Mohammed", "Ahmed", "ahmed", "Yuliia", "Chris"
+        ));
+
+        //remove all the names "ahmed"
+
+        names3.removeIf(each -> each.equalsIgnoreCase("ahmed"));
+
+        System.out.println(names3);
+
+
+
+
+
+
+    }
+
+}
